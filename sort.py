af381eb08d72332a3d22f80f89591a2fca1f38b2
heinrichI <jeronimo73@gmail.com>
2019-07-25 23:29:46 +0400

    sort

--- sort.py
+++ sort.py
@@ -5,9 +5,11 @@ import numpy as np
 import glob
 #import pathlib
 
+labels = ['Girls', 'LatexNoBondage', 'Mummification', 'New York', 'bdsm', 'best', 'Приколы', 'Рисованное']
+
 # returns a compiled model
 # identical to the previous one
-model = tf.keras.models.load_model('image_classification.h5')
+model = tf.keras.models.load_model('image_classification_fine.h5')
 
 """As the model trains, the loss and accuracy metrics are displayed. This model reaches an accuracy of about 0.88 (or 88%) on the training data.
 
@@ -18,7 +20,20 @@ Next, compare how the model performs on the test dataset:
 
 image_size = 160 # All images will be resized to 160x160
 batch_size = 32
-sort_dir='Sort'
+sort_dir='d:\Downloads3_LG\VK'
+
+def preprocess_image(image):
+  image = tf.image.decode_jpeg(image, channels=3)
+  image = tf.image.resize(image, [image_size, image_size])
+  image /= 255.0  # normalize to [0,1] range
+  #image = (image/127.5) - 1
+
+  return image
+
+def load_and_preprocess_image(path):
+  image = tf.read_file(path)
+  return preprocess_image(image)
+
 
 
 #data_root = pathlib.Path(sort_dir)
@@ -27,8 +42,23 @@ sort_dir='Sort'
 path = os.path.join(sort_dir, '*')
 all_image_paths = glob.glob(path)
 
+#labels = (model.)
 
+for image_paths in all_image_paths:
+    test_image = keras.preprocessing.image.load_img(image_paths, target_size=(image_size, image_size))
+    test_image = keras.preprocessing.image.img_to_array(test_image)
+    test_image = np.expand_dims(test_image, axis=0)
+    test_image.reshape(image_size, image_size, 3)
 
+    prediction = model.predict(test_image, steps = 1, batch_size=1)
+    predicted_class_indices = np.argmax(prediction,axis=1)
+    classS = labels[predicted_class_indices[0]]
+    #classS = np.array2string(predicted_class_indices)
+    target_dir = os.path.join(sort_dir, classS)
+    if not os.path.exists(target_dir):
+        os.mkdir(target_dir)
+    target_path = os.path.join(target_dir, os.path.basename(image_paths))
+    os.rename(image_paths, target_path)
 
 
 """## Build a `tf.data.Dataset`
@@ -40,37 +70,27 @@ The easiest way to build a `tf.data.Dataset` is using the `from_tensor_slices` m
 Slicing the array of strings results in a dataset of strings:
 """
 
-path_ds = tf.data.Dataset.from_tensor_slices(all_image_paths)
+#path_ds = tf.data.Dataset.from_tensor_slices(all_image_paths)
 
-"""The `output_shapes` and `output_types` fields describe the content of each item in the dataset. In this case it is a set of scalar binary-strings"""
+#"""The `output_shapes` and `output_types` fields describe the content of each item in the dataset. In this case it is a set of scalar binary-strings"""
 
-print('shape: ', repr(path_ds.output_shapes))
-print('type: ', path_ds.output_types)
-print()
-print(path_ds)
+#print('shape: ', repr(path_ds.output_shapes))
+#print('type: ', path_ds.output_types)
+#print()
+#print(path_ds)
 
-"""Now create a new dataset that loads and formats images on the fly by mapping `preprocess_image` over the dataset of paths."""
+#"""Now create a new dataset that loads and formats images on the fly by mapping `preprocess_image` over the dataset of paths."""
 
-def preprocess_image(image):
-  image = tf.image.decode_jpeg(image, channels=3)
-  image = tf.image.resize(image, [image_size, image_size])
-  #image /= 255.0  # normalize to [0,1] range
-  image = (image/127.5) - 1
 
-  return image
 
-def load_and_preprocess_image(path):
-  image = tf.read_file(path)
-  return preprocess_image(image)
+#AUTOTUNE = tf.data.experimental.AUTOTUNE
+#image_ds = path_ds.map(load_and_preprocess_image, num_parallel_calls=AUTOTUNE)
 
-AUTOTUNE = tf.data.experimental.AUTOTUNE
-image_ds = path_ds.map(load_and_preprocess_image, num_parallel_calls=AUTOTUNE)
+#test_datagen = keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
+#m=test_datagen.flow(image_ds, batch_size=32)
+#y_pred=model.predict_generator(m, 32)
 
-test_datagen = keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
-m=test_datagen.flow(image_ds, batch_size=32)
-y_pred=model.predict_generator(m, 32)
-
-predictions = model.predict(image_ds)
+#predictions = model.predict(image_ds)
 
 
 
@@ -174,23 +194,23 @@ With the model trained, we can use it to make predictions about some images.
 
 
 # Predict from generator (returns probabilities)
-pred=model.predict_generator(batch_generator, steps=len(test_generator), verbose=1)
+#pred=model.predict_generator(batch_generator, steps=len(test_generator), verbose=1)
 
-predicted_class_indices=np.argmax(pred,axis=1)
+#predicted_class_indices=np.argmax(pred,axis=1)
 
-labels = (test_generator.class_indices)
-labels = dict((v,k) for k,v in labels.items())
-predictions = [labels[k] for k in predicted_class_indices]
+#labels = (test_generator.class_indices)
+#labels = dict((v,k) for k,v in labels.items())
+#predictions = [labels[k] for k in predicted_class_indices]
 
-# Get classes by np.round
-#cl = np.round(pred)
-# Get filenames (set shuffle=false in generator is important)
-filenames=test_generator.filenames
+## Get classes by np.round
+##cl = np.round(pred)
+## Get filenames (set shuffle=false in generator is important)
+#filenames=test_generator.filenames
 
-for idx, filename in enumerate(filenames):
-     print(idx, filename, predictions[idx])
-     target_path = os.path.join(sort_dir, predictions[idx])
-     os.rename(filename, target_path)
+#for idx, filename in enumerate(filenames):
+#     print(idx, filename, predictions[idx])
+#     target_path = os.path.join(sort_dir, predictions[idx], filename)
+#     os.rename(filename, target_path)
 
 #predictions = model.predict(test_generator)
 
